48ce39318b7b015752dc7def8d5abf52c4ab047b
ChaCha3088 <cha3088@gmail.com>
2025-01-08 00:05:15 +0900

    Add: 25/01/08

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,42 +1,17 @@
-import { useState } from 'react';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
-import Header from './components/Header';
-import UserInput from './components/UserInput';
-import Results from './components/Results';
-
-function App() { 
-	const [userInput, setUserInput] = useState(
-		{
-			initialInvestment: 15000,
-			annualInvestment: 1200,
-			expectedReturn: 6,
-			duration: 10,
-		}
-	);
-
-	function handleChange(inputIdentifier, newValue) {
-		setUserInput(prevUserInput => {
-			if (inputIdentifier == "duration" && parseInt(newValue) <= 0) {
-				return {
-					...prevUserInput,
-					[inputIdentifier]: 0,
-				}
-			}
-
-			return {
-				...prevUserInput,
-				[inputIdentifier]: parseFloat(newValue),
-			};
-		});
-	}
-
-	return (
-	<>
-		<Header />
-		<UserInput userInput={userInput} handleChange={handleChange} />
-		<Results userInput={userInput} />
-	</>
-	);
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 01-starting-project/src/index.jsx
+++ 01-starting-project/src/index.jsx
@@ -1,6 +1,8 @@
-import ReactDOM from 'react-dom/client';
+import react from 'react';
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx';
-import './index.css';
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const entryPoint = document.getElementById("root");
+ReactDOM.createRoot(entryPoint).render(<App />);
