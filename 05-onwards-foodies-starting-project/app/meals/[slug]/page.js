07307ff29e93faf403d3b3cf96499d059822c661
ChaCha3088 <cha3088@gmail.com>
2025-01-14 17:49:17 +0900

    Add: 250114

---
+++ 05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function Meal({params}) {
+    return (
+        <main>
+            <h1>The Meals with {params.slug}</h1>
+        </main>
+    );
+}
---
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Meal() {
+    return (
+        <main>
+            <h1>The Meal</h1>
+            <p>
+                <Link href="/meals/share">Share</Link>
+            </p>
+            <p>
+                <Link href="/meals/1">Meals 1</Link>
+            </p>
+            <p>
+                <Link href="/meals/2">Meals 2</Link>
+            </p>
+        </main>
+    );
+}
