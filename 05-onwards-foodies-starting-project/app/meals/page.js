10f8173e28c9bda45f199bedce3b22fd3bbc6e1c
ChaCha3088 <cha3088@gmail.com>
2025-01-15 17:45:00 +0900

    Add: 250115

--- 05-onwards-foodies-starting-project/app/meals/page.js
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Meal() {
+export default function Meals() {
     return (
         <main>
             <h1>The Meal</h1>
--- 05-onwards-foodies-starting-project/components/main-header/main-header.js
+++ 05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -3,31 +3,41 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
+
     return (
         <>
             <MainHeaderBackground />
             <header className={classes.header}>
                 <Link className={classes.logo} href="/">
                     <Image
-                        src={logoImg} 
+                        src={logoImg}
                         alt="A plate with food on it"
                         priority
                     />
                         Next Level Food
                 </Link>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <NavLink 
+                                href="/meals"
+                            >
+                            Browse Meals
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                href="/community"
+                            >
+                            Foodie Community
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
             </header>
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href="/meals">Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href="/community">Foodie Community</Link>
-                    </li>
-                </ul>
-            </nav>
         </>
     );
 }
